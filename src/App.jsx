fc7f98df46473f71e4d4caf9d1766fceab171493
Gustavo-Henrique-da-Silva <gustavopereira8919@gmail.com>
2024-02-06 02:25:58 +0000

    Adicao pesquisa e pequenas correcoes

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+// App.jsx
+import React, { useState } from 'react';
 import './App.css';
 import Header from '../src/componentes/Header/Header';
 import Sidebar from './componentes/Sidebar/Sidebar';
@@ -5,12 +7,18 @@ import Footer from './componentes/Footer/Footer';
 import Main from './componentes/Main/Main';
 
 function App() {
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const handleSearch = (term) => {
+    setSearchTerm(term);
+  };
+
   return (
     <div>
       <Sidebar />
-      <Header />
-      <Main/>
-      <Footer/>
+      <Header onSearch={handleSearch} />
+      <Main searchTerm={searchTerm} />
+      <Footer />
     </div>
   );
 }
